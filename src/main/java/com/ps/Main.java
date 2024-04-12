1ef5e63cdd6406eea502c5533934189787a8223d
Alisha Yu <alisha.yu15@gmail.com>
2024-04-12 12:57:21 -0400

    added in principal, interest rate, and loan length

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,23 @@
 package com.ps;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+ Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the principal amount.");
+            int principal = scanner.nextInt();
+
+        System.out.println("Please enter the interest rate.");
+            double interestRate = scanner.nextDouble();
+
+        System.out.println("Please enter in the loan length in years.");
+            int loanLength = scanner.nextInt();
+
+
+
+
     }
 }
