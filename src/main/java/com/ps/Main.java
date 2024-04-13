4bb88a71720fd951d1f039be58840f822c252396
Alisha <alisha.yu15@gmail.com>
2024-04-12 20:09:19 -0400

    completed daily compound calc

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -9,13 +9,13 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please select which calculator you would like to use:");
-        System.out.println("Mortgage, Compound Interest, or Present Value");
+        System.out.println("Mortgage, Daily Compound Interest, or Present Value");
             String calculator = scanner.nextLine();
 
             if (calculator.equals("Mortgage")) {
                 mortgageCalculator(scanner);
             }
-            else if (calculator.equals("Compound Interest")) {
+            else if (calculator.equals("Daily Compound Interest")) {
                 compoundCalculator(scanner);
             }
             else if (calculator.equals("Present Value")) {
@@ -66,6 +66,27 @@ public class Main {
 
     public static void compoundCalculator(Scanner scanner) {
 
+        System.out.println("Please enter in the deposit amount.");
+            int principal = scanner.nextInt();
+
+        System.out.println("Please enter in the interest rate.");
+            float percentage = scanner.nextFloat();
+            float interestRate = percentage/100;
+
+        System.out.println("Please enter in the number of years you would like to invest for.");
+            int numYears = scanner.nextInt();
+
+/* Final Amount = Principal * [1+interestRate/365]^365*time */
+
+        double inner = 1+(interestRate/365);
+        float occurrence = numYears*365;
+        double exp = Math.pow(inner, occurrence);
+        double futureValue = principal*exp;
+
+
+        System.out.printf("Your Certificate of Deposit (CD)'s ending balance will be $%f",
+                futureValue
+        );
     }
 
     public static void pvCalculator(Scanner scanner) {
